453ff44072fb0a1f3987a78b04795f13342970f9
bchr1241 <bj.chavezr@alum.up.edu.pe>
2024-07-14 04:36:35 +0000

    Ejercicio subido

---
+++ Ejercicio9.py
@@ -0,0 +1,12 @@
+#Escriba un programa que, dada una lista, devuelve una nueva lista cuyo contenido sea igual a la
+#original pero invertida. Así, dada la lista ['Di', 'buen', 'día', 'a', 'papa'], deberá devolver ['papa', 'a',
+#'día', 'buen', 'Di'].
+
+lista1 = ["Hola", "a", "todos", "los", "presentes"]
+print("Este es el orden normal de la lista:")
+print(lista1)
+
+lista1.reverse()
+print("Este es el orden invertido de la lista usando reverse():")
+print(lista1)
+
