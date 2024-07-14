0ad88af543c0756a89c28a25257fce672a226f58
bchr1241 <bj.chavezr@alum.up.edu.pe>
2024-07-14 03:23:09 +0000

    Ejercicio subido

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,2 +1,6 @@
+#Escribir un programa que solicite su nombre de usuario por consola y después de que el usuario lo
+#introduzca muestre por pantalla la cadena “¡Hola <nombre>!”, donde <nombre> es el nombre que el
+#usuario haya introducido.
+
 nombre = input("Ingrese nombre: ")
 print("¡Hola ", nombre, "!")
